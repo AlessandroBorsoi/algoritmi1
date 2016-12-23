a9437438b3a4577989112fce55d2f816008736fc
Alessandro Borsoi <alessandroborsoi@MacBook-Pro.local>
2016-12-23 15:00:17 +0100

    MergeSort fixed

--- src/sort.c
+++ src/sort.c
@@ -29,16 +29,15 @@
 
 void upo_insertion_sort(void* base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
-    char* array = base;
     size_t i, j;
-    for (i = 1; i < n - 1; ++i)
+    for (i = 1; i < n; ++i)
     {
         j = i;
-        while (j > 0 && cmp(upo_get_array_element(array, j - 1, size), upo_get_array_element(array, j, size)))
+        while (j > 0 && cmp(upo_get_array_element(base, j - 1, size), upo_get_array_element(base, j, size)) > 0)
         {
             upo_swap(
-                upo_get_array_element(array, j - 1, size), 
-                upo_get_array_element(array, j, size), 
+                upo_get_array_element(base, j - 1, size), 
+                upo_get_array_element(base, j, size), 
                 size);
             --j;
         }
@@ -84,18 +83,40 @@ static void upo_merge_sort_merge(void* base, size_t lo, size_t mid, size_t hi, s
     void* aux;
     i = 0;
     j = mid + 1 - lo;
-    aux = malloc(size * (hi - lo));
+    aux = malloc(size * (hi - lo + 1));
     upo_copy_array(aux, base, lo, hi, size);
     for (k = lo; k <= hi; ++k)
     {
-        if (i > mid - lo || cmp(upo_get_array_element(aux, j, size), upo_get_array_element(aux, i, size)))
+        if (i > (mid - lo))
         {
-            upo_copy_array_element(base, aux, size);
+            upo_copy_array_element(
+                upo_get_array_element(base, k, size), 
+                upo_get_array_element(aux, j, size), 
+                size);
+            ++j;
+        }
+        else if (j > (hi - lo))
+        {
+            upo_copy_array_element(
+                upo_get_array_element(base, k, size), 
+                upo_get_array_element(aux, i, size), 
+                size);
+            ++i;
+        }
+        else if (cmp(upo_get_array_element(aux, j, size), upo_get_array_element(aux, i, size)) < 0)
+        {
+            upo_copy_array_element(
+                upo_get_array_element(base, k, size), 
+                upo_get_array_element(aux, j, size), 
+                size);
             ++j;
         }
         else
         {
-            upo_copy_array_element(base, aux, size);
+            upo_copy_array_element(
+                upo_get_array_element(base, k, size), 
+                upo_get_array_element(aux, i, size), 
+                size);
             ++i;
         }
     }
@@ -104,25 +125,24 @@ static void upo_merge_sort_merge(void* base, size_t lo, size_t mid, size_t hi, s
 
 static void upo_copy_array(void* dest, void* src, size_t lo, size_t hi, size_t size)
 {
-    size_t i;
-    size_t n = hi - lo;
-    for (i = 0; i < n; ++i)
+    size_t i, j;
+    for (i = 0, j = lo; j <= hi; ++i, ++j)
     {
         upo_copy_array_element(
             upo_get_array_element(dest, i, size), 
-            upo_get_array_element(src, i, size), 
+            upo_get_array_element(src, j, size), 
             size);
     }
 }
 
 static void upo_copy_array_element(void* dest, void* src, size_t size)
 {
-    char* pc1 = dest;
-    char* pc2 = src;
+    char* to = dest;
+    char* from = src;
     size_t i;
     for (i = 0; i < size; ++i)
     {
-        pc1[i] = pc2[i];
+        to[i] = from[i];
     }
 }
 
@@ -152,11 +172,11 @@ static size_t partition(void* base, size_t lo, size_t hi, size_t size, upo_sort_
         do 
         {
             ++i;
-        } while (i >= hi || cmp(upo_get_array_element(base, p, size), upo_get_array_element(base, i, size)));
+        } while (i >= hi || cmp(upo_get_array_element(base, i, size), upo_get_array_element(base, p, size)) >= 0);
         do 
         {
             --j;
-        } while (j <= lo || cmp(upo_get_array_element(base, j, size), upo_get_array_element(base, p, size)));
+        } while (j <= lo || cmp(upo_get_array_element(base, j, size), upo_get_array_element(base, p, size)) <= 0);
         if (i >= j) break;
         upo_swap(upo_get_array_element(base, i, size), upo_get_array_element(base, j, size), size);
     }
